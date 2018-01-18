3d7ca5d2ba7545cb2dd4eea996c8e1960db53dc1
BardoBravo <claudiovtramos@gmail.com>
2018-01-17 23:59:25 -0200

    db connection 2

--- routes/users.js
+++ routes/users.js
@@ -10,55 +10,82 @@ var router = express.Router();
 	});
 } */
 
-function joinObjects() {
-  console.log("iii");
-  var idMap = {};
-  // Iterate over arguments
-  for(var i = 0; i < arguments.length; i++) { 
-    // Iterate over individual argument arrays (aka json1, json2)
-    for(var j = 0; j < arguments[i].length; j++) {
-       var currentID = arguments[i][j]['char'];
-       if(!idMap[currentID]) {
-          idMap[currentID] = {};
-        }
-       // Iterate over properties of objects in arrays (aka id, name, etc.)
-      for(key in arguments[i][j]) {
-          idMap[currentID][key] = arguments[i][j][key];
-      }
-    }
-  }
-  // push properties of idMap into an array
-  var newArray = [];
-  for(property in idMap) {
-    newArray.push(idMap[property]);
-  }
-  return newArray;
-};  
+// function joinObjects() {
+//   console.log("iii");
+//   var idMap = {};
+//   // Iterate over arguments
+//   for(var i = 0; i < arguments.length; i++) { 
+//     // Iterate over individual argument arrays (aka json1, json2)
+//     for(var j = 0; j < arguments[i].length; j++) {
+//        var currentID = arguments[i][j]['char'];
+//        if(!idMap[currentID]) {
+//           idMap[currentID] = {};
+//         }
+//        // Iterate over properties of objects in arrays (aka id, name, etc.)
+//       for(key in arguments[i][j]) {
+//           idMap[currentID][key] = arguments[i][j][key];
+//       }
+//     }
+//   }
+//   // push properties of idMap into an array
+//   var newArray = [];
+//   for(property in idMap) {
+//     newArray.push(idMap[property]);
+//   }
+//   return newArray;
+// };  
 
 
 /* GET users listing. */
 router.get('/userlist', function(req, res) {
-	var db = req.db;
-	//var collection = db.get('userlist');
-	var collection = db.get('overall');
-	collection.find({},{},function(e,docs) {
-		res.json(docs);
-	})
+	// var db = req.db;
+	// //var collection = db.get('userlist');
+	// var collection = db.get('overall');
+	// collection.find({},{},function(e,docs) {
+	// 	res.json(docs);
+	// });
+
+	var path = req.uri;
+	req.MongoClient.connect(path, function(err, db) {
+   		const collection = db.db("Runbound").collection("overall");   
+   		var cursor = collection.find({},{});   		
+		cursor.toArray(function (err, data){
+    	if(err) {
+      		return console.log(err);
+    	}
+    		res.json(data);
+   		} );
+
+   		db.close();
+	
+	});
 });
 
 /* GET users listing. */
 router.get('/playerPerGame/:play_ID', function(req, res) {
-	var db = req.db;	
-	var jsonDocs;
-	var playToShow;
-	playToShow = req.params.play_ID;
-	var collection = db.get('test1');		
-	collection.find({ 'play_Id' : playToShow },{},function(e,docs) {
-		jsonDocs = docs;
-		string = JSON.stringify(docs);
-		console.log(string);
-		res.json(docs);
-	});
+	// var db = req.db;	
+	// var playToShow;
+	// playToShow = req.params.play_ID;
+	// var collection = db.get('test1');		
+	// collection.find({ 'play_Id' : playToShow },{},function(e,docs) {		
+	// 	res.json(docs);
+	// });
+
+	var playToShow = req.params.play_ID;
+	var path = req.uri;
+	req.MongoClient.connect(path, function(err, db) {
+   		const collection = db.db("Runbound").collection("gamePlayers");   
+   		var cursor = collection.find({ 'play_Id' : playToShow },{});   		
+		cursor.toArray(function (err, data){
+    	if(err) {
+      		return console.log(err);
+    	}
+    		res.json(data);
+   		} );
+
+   		db.close();
+	
+	});		
 
 });
 
@@ -74,24 +101,51 @@ router.post('/adduser', function(req, res) {
 });
 
 router.post('/addGame', function(req, res) {
-	var db = req.db;
-	var collection = db.get('playData');	
-	collection.insert(req.body, function(err, result) {
-		res.send(
-			(err === null) ? { msg: ''} : { msg: err } 
-			);
-	});
+	// var db = req.db;
+	// var collection = db.get('playData');	
+	// collection.insert(req.body, function(err, result) {
+	// 	res.send(
+	// 		(err === null) ? { msg: ''} : { msg: err } 
+	// 		);
+	// });
+
+	var path = req.uri;
+	req.MongoClient.connect(path, function(err, db) {
+   		const collection = db.db("Runbound").collection("playData");      		
+		collection.insert(req.body, function(err, result) {
+			res.send(
+				(err === null) ? { msg: ''} : { msg: err } 
+				);
+		});
+
+   		db.close();
+	
+	});		
 });
 
 router.post('/addPlayerInGame', function(req, res) {
-	var db = req.db;
-	var collection = db.get('playerPerGame');	
-	console.log(req.body);
-	collection.insert(req.body, function(err, result) {
-		res.send(
-			(err === null) ? { msg: ''} : { msg: err } 
-			);
-	});
+	// var db = req.db;
+	// var collection = db.get('playerPerGame');	
+	// console.log(req.body);
+	// collection.insert(req.body, function(err, result) {
+	// 	res.send(
+	// 		(err === null) ? { msg: ''} : { msg: err } 
+	// 		);
+	// });
+
+	var path = req.uri;
+	req.MongoClient.connect(path, function(err, db) {
+   		const collection = db.db("Runbound").collection("playerPerGame");   	
+		collection.insert(req.body, function(err, result) {
+			res.send(
+				(err === null) ? { msg: ''} : { msg: err } 
+				);
+		});
+
+   		db.close();
+	
+	});	
+
 });
 
 //DELETE 
@@ -107,21 +161,52 @@ router.delete('/deleteuser/:id', function(req, res) {
 
 //cahr
 router.get('/char/:index', function(req, res) {
-	var db = req.db;
-	var collection = db.get('char');
+	// var db = req.db;
+	// var collection = db.get('char');
+	// var charID = req.params.index;
+	// collection.find({ 'index' : charID},{}, function(e,docs) {
+	// 	res.json(docs);
+	// });
+
 	var charID = req.params.index;
-	collection.find({ 'index' : charID},{}, function(e,docs) {
-		res.json(docs);
-	});
+	var path = req.uri;
+	req.MongoClient.connect(path, function(err, db) {
+   		const collection = db.db("Runbound").collection("char");   
+   		var cursor = collection.find({'index' : charID},{});   		
+		cursor.toArray(function (err, data){
+    	if(err) {
+      		return console.log(err);
+    	}
+    		res.json(data);
+   		} );
+
+   		db.close();
+	
+	});	
 });
 
 //cahr
 router.get('/char/', function(req, res) {
-	var db = req.db;
-	var collection = db.get('char');
-	collection.find({},{}, function(e,docs) {
-		res.json(docs);
-	});
+	// var db = req.db;
+	// var collection = db.get('char');
+	// collection.find({},{}, function(e,docs) {
+	// 	res.json(docs);
+	// });
+
+	var path = req.uri;
+	req.MongoClient.connect(path, function(err, db) {
+   		const collection = db.db("Runbound").collection("char");   
+   		var cursor = collection.find({},{});   		
+		cursor.toArray(function (err, data){
+    	if(err) {
+      		return console.log(err);
+    	}
+    		res.json(data);
+   		} );
+
+   		db.close();
+	
+	});	
 });
 
 
@@ -178,19 +263,49 @@ router.get('/scenarioList/', function(req, res) {
 
 //equipments
 router.get('/equipList/', function(req, res) {
-	var db = req.db;
-	var collection = db.get('equipment');
-	collection.find({},{}, function(e,docs) {
-		res.json(docs);
+	// var db = req.db;
+	// var collection = db.get('equipment');
+	// collection.find({},{}, function(e,docs) {
+	// 	res.json(docs);
+	// });
+
+	var path = req.uri;
+	req.MongoClient.connect(path, function(err, db) {
+   		const collection = db.db("Runbound").collection("equipment");   
+   		var cursor = collection.find({},{});   		
+		cursor.toArray(function (err, data){
+    	if(err) {
+      		return console.log(err);
+    	}
+    		res.json(data);
+   		} );
+
+   		db.close();
+	
 	});
 });
 
 //equipments
 router.get('/skillList/', function(req, res) {
-	var db = req.db;
-	var collection = db.get('skills');
-	collection.find({},{}, function(e,docs) {
-		res.json(docs);
+	// var db = req.db;
+	// var collection = db.get('skills');
+	// collection.find({},{}, function(e,docs) {
+	// 	res.json(docs);
+	// });
+
+	var path = req.uri;
+	req.MongoClient.connect(path, function(err, db) {
+   		const collection = db.db("Runbound").collection("skills");   
+   		var cursor = collection.find({},{});   		
+		cursor.toArray(function (err, data){
+    	if(err) {
+      		return console.log(err);
+    	}
+    		res.json(data);
+   		} );
+
+   		db.close();
+	
 	});
 });
 
